444c6a0e9505ea841b05691e4ecc6e687968da7b
zhyx-11 <750537172@qq.com>
2021-10-13 15:17:11 +0800

    Create login.js

---
+++ static/js/login.js
@@ -0,0 +1,8 @@
+function jump(){
+    var a = document.getElementById("user").value;
+    var b = document.getElementById("pwd").value;
+   if ((a == 123) && b == 123) {
+        window.location.href="./templates/picture.html";
+    }  
+  }
+  
