a4e97a950ca55834c955e1ab2a9d30b769143123
zhyx-11 <750537172@qq.com>
2021-10-15 17:10:32 +0800

    Update login.js

--- static/js/login.js
+++ static/js/login.js
@@ -1,8 +1,8 @@
 function jump(){
     var a = document.getElementById("user").value;
     var b = document.getElementById("pwd").value;
-   if ((a == 123) && b == 123) {
-        window.location.href="./templates/picture.html";
+   if ((a == 15938788961) && b == 971011) {
+        window.location.href="./templates/homepage.html";
     }  
   }
   
