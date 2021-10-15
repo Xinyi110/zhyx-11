b8fb5609ab0981fb7c2a0b62c62976f742c1cb26
zhyx-11 <750537172@qq.com>
2021-10-15 21:57:54 +0800

    Update login.js

--- static/js/login.js
+++ static/js/login.js
@@ -3,6 +3,8 @@ function jump(){
     var b = document.getElementById("pwd").value;
    if ((a == 15938788961) && b == 971011) {
         window.location.href="./templates/homepage.html";
-    }  
+    }else{
+      alert("账号或者密码错误1")
+    }
   }
   
