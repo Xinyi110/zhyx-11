ca3cfd7763bf96bb2a99366a21e38c474c390bee
zhyx-11 <750537172@qq.com>
2022-02-21 11:08:08 +0800

    Create 333.php

---
+++ templates/333.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> sadasdsada </title>
+</head>
+<body> 
+    <table>
+        <tr>    
+        <th>user</th>
+        <th>id</th>
+        </tr>
+        <?php
+            // $conn = mysqli_connect("localhost","root","")
+            echo "hello";
+        ?>
+    </table>
+</body>
