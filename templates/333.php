71fa5e414d404287b13b90803d546264f5da2c72
zhyx-11 <750537172@qq.com>
2022-02-28 16:50:29 +0800

    Update 333.php

--- templates/333.php
+++ templates/333.php
@@ -1,17 +1,12 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title> sadasdsada </title>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
-<body> 
-    <table>
-        <tr>    
-        <th>user</th>
-        <th>id</th>
-        </tr>
-        <?php
-            // $conn = mysqli_connect("localhost","root","")
-            echo "hello";
-        ?>
-    </table>
+<body>
+    
 </body>
+</html>
